e17fbb836a42563f7fb25aca3e3ada66c6724168
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-10-13 21:19:54 +0300

    Create 0063.pas

---
+++ 0063.pas
@@ -0,0 +1,20 @@
+﻿program p2;
+
+var
+  S, P, X, Y: integer;
+
+begin
+  
+  readln(S, P);
+  
+  for X := 0 to S do
+  begin
+    Y := S - X;
+    if X * Y = P then
+      begin
+      writeln(X, ' ', Y);
+      break;
+      end;
+  end;
+  
+end.
