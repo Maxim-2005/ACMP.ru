d50d0aea92e26415f3c4409d35c8616d7d3ed36c
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-10-18 22:01:06 +0300

    697 and 777

---
+++ 697.pas
@@ -0,0 +1,16 @@
+﻿program p1;
+
+var L, W, H, S, B : integer;
+
+begin
+  
+  readln(L, W, H);
+  S := (L + W) * H * 2;
+  B := S div 16;
+  
+  if S mod 16 > 0 then
+    B := B + 1;
+  
+  writeln(B);
+  
+end.
---
+++ 777.pas
@@ -0,0 +1,14 @@
+﻿program p1;
+
+var
+  S, T, A: integer;
+begin
+  readln(S, T);
+
+  if S < T then
+    A := T - S
+  else
+    A := T - S + 12;
+  
+  writeln(A);
+end.
