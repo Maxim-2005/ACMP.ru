1d70f2262c0bc421ddd2300f9564c2a56e233fbd
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-10-19 20:44:15 +0300

    afdfd

---
+++ 0529.pas
@@ -0,0 +1,16 @@
+﻿program p1;
+
+var X1, Y1, X2, Y2, S, a, b, c, d :real;
+
+begin
+  
+  readln(X1, Y1, X2, Y2);
+  
+  a := X1 - X2;
+  b := Y1 - Y2;
+  
+  S := sqrt(a * a + b * b);
+  
+  Writeln(S:0:5);
+  
+end.
