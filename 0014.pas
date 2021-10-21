39eb8fb42569280727d067bf3f8640e53c349618
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-10-21 21:11:47 +0300

    Create 0014.pas

---
+++ 0014.pas
@@ -0,0 +1,17 @@
+﻿program p1;
+
+var
+  a, b, N, i: integer;
+
+begin
+  
+  readln(a, b);
+  
+  for i := 1 to a * b do
+  begin
+    N := a * i;
+    if N mod b = 0 then
+      break;
+  end;
+  writeln(N);
+end.
