2da034059d4b39b2cc0c97b61741afd0d8afe73d
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2021-10-25 22:14:34 +0300

    Update 0854.py

--- 0854.py
+++ 0854.py
@@ -1,17 +1,11 @@
-A, B = list(map(int, input().split()))
-mode = str(input())
-if A < B and mode == "heat":
-	T = B
-elif A > B and mode == "heat":
-	T = A
-elif A < B and mode == "auto":
-	T = B
-elif A > B and mode == "auto":
-	T = B
-elif A > B and mode == "freeze":
-	T = B
-elif A < B and mode == "freeze":
-	T = A
-elif mode == "fan":
-	T = A
-print(T)
+A, B = map(int, input().split())
+mode = input()
+
+if mode == "freeze":
+        print(min(A, B))
+elif mode == "heat":
+        print(max(A, B))
+elif mode == "auto":
+        print(B)
+else:
+        print(A)
