39555ca46f8a54c787350124db10bf4d93fe657f
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-01-14 21:07:18 +0300

    328

---
+++ 0328.py
@@ -0,0 +1,10 @@
+N = int(input())
+print(N * (N + 1) * (N + 2) // 2) 
+
+##for i in range(N+1):
+##    for j in range(i, N + 1):
+##        S += i + j
+
+## ПЕРЕБОР С АЛГОРИТМОМ
+##for i in range(N+1):
+##    S += i * (N - i + 1) + ((i + N) * (N - i + 1) // 2)
