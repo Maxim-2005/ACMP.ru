96cb0b40af99d41645e51b16cfacf97eabe13af7
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-10-03 20:52:48 +0300

    Create 0143.pas

---
+++ 0143.pas
@@ -0,0 +1,17 @@
+﻿program p0147;
+
+var
+  N, x, x1, x2: integer;
+
+begin
+  Readln(N);
+  x1 := 0;
+  x2 := 1;
+  for var i := 0 to N-1 do
+  begin
+    x := x1 + x2;
+    x2 := x1;
+    x1 := x;
+  end;
+    writeln(x);
+end.
