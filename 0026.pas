ff9e263ff904954a226f844261e0092c1e8d13f7
Maxim-2005 <55892094+Maxim-2005@users.noreply.github.com>
2022-10-04 20:44:56 +0300

    Create 0026.pas

---
+++ 0026.pas
@@ -0,0 +1,17 @@
+﻿program p0026;
+
+var
+  x1, y1, r1, x2, y2, r2: integer;
+  g: real;
+
+begin
+  readln(x1, y1, r1);
+  readln(x2, y2, r2);
+  
+  g := sqrt(sqr(x2 - x1) + sqr(y2 - y1));
+  
+  if (r1 + r2 >= g) and (abs(r1 - r2) <= g) then
+    writeln('YES')
+  else
+    writeln('NO');
+end.
